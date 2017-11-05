192c4a96c68170cfcfd0d8855bf00b6efe457de7
健太 荻野 <kenogi1025@gmail.com>
2017-11-06 01:06:53 +0900

    プロジェクト ファイルを追加します。

---
+++ th_002/title.cpp
@@ -0,0 +1,20 @@
+#include "title.h"
+#include <Siv3D.hpp>
+
+typedef enum{
+	titlemenu,
+	gamestart,
+	result,
+	config,
+	quit
+}STATE;
+
+title::title(){
+}
+
+void title::menu(STATE state_) {
+
+}
+
+title::~title(){
+}
---
+++ th_002/title.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern enum STATE;
+class title{
+public:
+	title();
+	void menu(STATE state_);
+	~title();
+};
+
