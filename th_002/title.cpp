05c2430371f679f8b3c9d7bf2cb8900fbcd78065
健太 荻野 <kenogi1025@gmail.com>
2017-11-06 16:32:39 +0900

    2017/11/6 16:32

--- th_002/title.cpp
+++ th_002/title.cpp
@@ -1,19 +1,78 @@
 #include "title.h"
 #include <Siv3D.hpp>
 
-typedef enum{
+typedef enum STATE{
 	titlemenu,
 	gamestart,
 	result,
 	config,
-	quit
+	quit,
+	all
 }STATE;
 
 title::title(){
+	state_num = titlemenu;
 }
 
-void title::menu(STATE state_) {
+int title::select(int &statenum, int itemnumber, int defaultitem) {
+	/* init */
+	struct element{
+		Point point;
+		Texture tex;
+	};
+	element selectmenu[4] = {
+		{ { 200,220 },Texture(L"pic/title/element/gamestart.dds") },
+		{ { 200,220 },Texture(L"pic/title/element/result.dds") },
+		{ { 200,220 },Texture(L"pic/title/element/config.dds") },
+		{ { 200,220 },Texture(L"pic/title/element/") }
+	};
+}
 
+void title::menu(STATE state_ = titlemenu) {
+	int *selectnum;
+	select(*selectnum, all,state_num);
+
+	switch (state_) {
+	case titlemenu:
+		state_num = titlemenu;
+		if (Input::KeyZ.pressed) {
+			switch (*selectnum) {
+			case gamestart:
+				menu(gamestart);
+				break;
+			case result:
+				menu(result);
+				break;
+			case config:
+				menu(config);
+				break;
+			case quit:
+				menu(quit);
+				break;
+			}
+		}
+		break;
+
+	case gamestart:
+		state_num = gamestart;
+		//gamestart
+		break;
+
+	case result:
+		state_num = result;;
+		//result
+		break;
+
+	case config:
+		state_num = config;
+		//config
+		break;
+
+	default:
+		System::Exit();
+		break;
+
+	}
 }
 
 title::~title(){
--- th_002/title.h
+++ th_002/title.h
@@ -1,10 +1,14 @@
 #pragma once
 
 extern enum STATE;
+
 class title{
+private:
+	int state_num;
 public:
 	title();
 	void menu(STATE state_);
+	int select(int &statenum,int itemnumber,int defaultitem);
 	~title();
 };
 
